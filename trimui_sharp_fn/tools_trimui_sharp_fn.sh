5f239a3db4438305bd159e585eb1e7c09fe2ea43
Andri <andriniaina@gmail.com>
2025-02-15 21:00:11 +0100

    lost

---
+++ trimui_sharp_fn/tools_trimui_sharp_fn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/userdata/system/trimui_sharp_fn/trimui_sharp_fn.sh
--- trimui_sharp_fn/trimui_sharp_fn.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-SDL_GAMECONTROLLERCONFIG="030000005e0400008e02000014010000,TRIMUI Player1,platform:Linux,b:b1,a:b0,dpdown:h0.4,leftx:a0,lefty:a1,rightx:a3,righty:a4,lefttrigger:a2,dpleft:h0.8,rightshoulder:b5,leftshoulder:b4,righttrigger:a5,dpright:h0.2,back:b6,start:b7,dpup:h0.1,y:b3,x:b2,guide:b8," /userdata/system/trimui_sharp_fn/trimui_sharp_fn
